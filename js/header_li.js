a88ebfabd090b109e9addc0c78c7a6486c0841a6
wuzhiyi <wzycrtd@163.com>
2015-11-17 20:06:30 +0800

    更改主页的一些布局细节；更改音乐播放器，新增菜单效果

---
+++ js/header_li.js
@@ -0,0 +1,30 @@
+/**
+ * Created by wuzhiyi on 2015/11/16.
+ */
+
+(function addA(){
+    var lis = document.getElementsByClassName("nav_li");
+    for (var i = 0; i < lis.length ; i++) {
+        (function (num) {
+            lis[num].onmouseover = function () {
+                var as = lis[num].childNodes;
+                var as = filterSpaceNode(as);
+                //alert(as[0].tagName); //a1
+                as[0].setAttribute("class", "inA");
+            }
+            lis[num].onmouseout = function(){
+                var as = lis[num].childNodes;
+                var as = filterSpaceNode(as);
+                as[0].removeAttribute("class","inA");
+                as[0].setAttribute("class","nav_ul_a");
+                //alert(as[0].tagName); //a1
+//                as[0].removeAttribute("class", "inA");
+
+            }
+        })(i);
+    }
+})();
+
+
+
+
